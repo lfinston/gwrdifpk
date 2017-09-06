9399c96dc84d894fca97dcd667873c514f8527a1
Laurence Finston <lfinston@users.noreply.github.com>
2017-09-06 15:45:25 +0200

    Initial upload.

---
+++ gwrdifpk.c
@@ -0,0 +1,54 @@
+/* gwrdifpk.c  */
+/* Created by Laurence D. Finston (LDF) Tue Jun 26 10:28:19 CEST 2012  */
+
+/* * (1) Copyright and License.
+
+This file is part of gwrdifpk, a package for long-term archiving.  
+Copyright (C) 2013, 2014 Gesellschaft fuer wissenschaftliche Datenverarbeitung mbH Goettingen  
+
+gwrdifpk is free software; you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation; either version 3 of the License, or 
+(at your option) any later version.  
+
+gwrdifpk is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details.  
+
+You should have received a copy of the GNU General Public License 
+along with gwrdifpk; if not, write to the Free Software 
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+gwrdifpk is available for downloading via Git from 
+https://github.com/gwdg/gwrdifpk.git.
+
+Please send bug reports to Laurence.Finston@gwdg.de
+
+The author can be contacted at: 
+
+Laurence D. Finston 
+Gesellschaft fuer wissenschaftliche Datenverarbeitung mbH Goettingen 
+Am Fassberg 11 
+37077 Goettingen  
+Germany
+
+Laurence.Finston@gwdg.de 
+*/
+
+
+/* Dummy C file.  Referred to in `configure.ac' and `Makefile.am'.  
+   It's helpful for it to exist, since it never needs to be changed or renamed.
+   LDF 2012.06.26.
+
+   That is, it never has to be renamed, unless the name of the package changes, 
+   as it did today.
+   LDF 2012.07.25.
+
+*/
+
+int
+main(int argc, char *argv[])
+{
+   return 0;
+}
